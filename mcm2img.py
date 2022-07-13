0c63c569b497d2bd9998790abfb6b2c65018f727
bri3d <brian@brianledbetter.com>
2022-07-13 17:29:34 -0600

    hd support

--- mcm2img.py
+++ mcm2img.py
@@ -2,7 +2,14 @@ from pathlib import Path
 from PIL import Image, ImageDraw
 from sys import argv
 
-out = Image.new("RGBA", (12, 4608), (0, 0, 0, 0))
+resize_factor = 2
+
+char_width = 12
+char_height = 18
+num_chars_max = 512
+chars_per_page = 256
+
+out = Image.new("RGBA", (char_width, char_height * num_chars_max), (0, 0, 0, 0))
 draw = ImageDraw.Draw(out)
 char = 0
 x = 0
@@ -33,25 +40,38 @@ with open(argv[1]) as f:
             continue
         for pixel in (line[i:i+2] for i in range(0, len(line), 2)):
             if pixel == "00": # Black
-                draw.point((x ,y + (char * 18)), (0, 0, 0, 255))
+                draw.point((x ,y + (char * char_height)), (0, 0, 0, 255))
             if pixel == "01": # Transparent
-                draw.point((x ,y + (char * 18)), (0, 0, 0, 0))
+                draw.point((x ,y + (char * char_height)), (0, 0, 0, 0))
             if pixel == "10": # White
-                draw.point((x, y + (char * 18)), (font_r, font_g, font_b, 255))
+                draw.point((x, y + (char * char_height)), (font_r, font_g, font_b, 255))
             if pixel == "11": # Transparent + White
-                draw.point((x, y + (char * 18)), (font_r, font_g, font_b, 0))
+                draw.point((x, y + (char * char_height)), (font_r, font_g, font_b, 0))
             x += 1
-            if x == 12:
+            if x == char_width:
                 y += 1
                 x = 0
-            if y == 18:
+            if y == char_height:
                 skip = 10
                 char += 1
                 y = 0
 
-out = out.resize((36, 13824), Image.NEAREST)
+font1 = out.crop((0, 0, char_width, char_height * chars_per_page))
+font2 = out.crop((0, char_height * chars_per_page, char_width, chars_per_page * 2 * char_height))
+
+resize_factor = 2
+out1_hd = font1.resize((char_width * resize_factor, char_height * chars_per_page * resize_factor), Image.NEAREST)
+out2_hd = font2.resize((char_width * resize_factor, char_height * chars_per_page * resize_factor), Image.NEAREST)
+
+resize_factor = 3
+out1 = font1.resize((char_width * resize_factor, char_height * chars_per_page * resize_factor), Image.NEAREST)
+out2 = font2.resize((char_width * resize_factor, char_height * chars_per_page * resize_factor), Image.NEAREST)
 
 if len(argv) > 3:
-    Path(argv[2]).write_bytes(out.tobytes('raw',argv[3]))
+    Path(argv[2] + "_hd.bin").write_bytes(out1_hd.tobytes('raw',argv[3]))
+    Path(argv[2] + "_hd_2.bin").write_bytes(out2_hd.tobytes('raw',argv[3]))
+    Path(argv[2] + ".bin").write_bytes(out1.tobytes('raw',argv[3]))
+    Path(argv[2] + "_2.bin").write_bytes(out2.tobytes('raw',argv[3]))
 else:
-    out.save(argv[2])
+    out1.save(argv[2])
+    out2.save("2_" + argv[2])
